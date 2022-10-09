3bdaa5eaa1cff4e2eb380a44038c0114af5cb3bf
Rishi <rishi.max95@gmail.com>
2022-10-09 09:46:18 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,18 @@ import "../styles/App.css";
 import { useState, useEffect } from "react";
 const App = () => {
   const [name, setName] = useState([]);
+  const [property, setProperty] = useState([]);
   useEffect(() => {
     fetch("https://content.newtonschool.co/v1/pr/main/users").then((result) => {
       result.JSON().then((resp) => {
         setName(resp);
+      });
+    });
+  }, []);
+  useEffect(() => {
+    fetch("https://content.newtonschool.co/v1/pr/main/users").then((result) => {
+      result.JSON().then((resp) => {
+        setProperty(resp);
       });
     });
   }, []);
